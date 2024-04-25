7714f7863f37c4ade1705962fcc021e7d333008c
FerLuque16 <feerluque77@gmail.com>
2024-04-25 01:58:03 -0300

    Lista la funcionalidad del ahorcado

--- src/app/components/juego/juego.component.ts
+++ src/app/components/juego/juego.component.ts
@@ -1,32 +1,104 @@
 import { Component } from '@angular/core';
 import { CategoriaService } from '../../services/categoria.service';
+import { CommonModule } from '@angular/common';
+
+interface Categorias {
+  capitales: string[];
+  paises: string[];
+  series: string[];
+  peliculas: string[];
+  animales: string[];
+}
 
 @Component({
   selector: 'app-juego',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './juego.component.html',
   styleUrl: './juego.component.css'
 })
 export class JuegoComponent {
 
-  categorias = {
-    capitales: ["Buenos Aires", "Washington D.C.", "París", "Tokio", "Canberra"],
-    paises: ["Argentina", "Estados Unidos", "Francia", "Japón", "Australia"],
-    series: ["Game of Thrones", "Stranger Things", "Breaking Bad", "Friends", "The Mandalorian"],
-    peliculas: ["The Shawshank Redemption", "Inception", "The Godfather", "Pulp Fiction", "The Dark Knight"],
-    animales: ["Perro", "Gato", "León", "Elefante", "Tigre"]
-  }
+  // categoriaSeleccionada:string ='';
+  categoriaSeleccionada:string ='series';
 
-  palabra = ['R', 'e', 'i', 'n', 'o', ' ', 'U', 'n', 'i', 'd', 'o'];
-  // palabra = ['a','r','g','e','n','t','i','n','a'];
+  palabra :string [] = [];
+  palabraAAdivinar : string = '';
+  palabraAMostrar : string = '';
+  listaDePalabras : string[]= [];
+  letrasPresionadas : string [] = [];
+  intentosRestantes: number = 5;
 
   letras = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m","n",
             "ñ","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
+  categorias:Categorias = {
+    capitales: ["Buenos Aires", "Washington", "París", "Tokio", "Canberra", "Brasilia"],
+    paises: ["Argentina", "Estados Unidos", "Francia", "Japón", "Australia"],
+    series: ["Game of Thrones", "Stranger Things", "Breaking Bad", "Friends", "The Mandalorian"],
+    peliculas: ["Los Vengadores", "Inception", "El padrino", "Pulp Fiction", "The Dark Knight"],
+    animales: ["Perro", "Gato", "León", "Elefante", "Tigre"]
+  }
+  
             
   constructor(private categoriaService: CategoriaService) {
-    console.log(categoriaService.getCategoria());
+    this.categoriaSeleccionada = categoriaService.getCategoria();
+    this.iniciarPalabraMostrada();
+
+  }
+
+  iniciarPalabraMostrada(){
+    this.listaDePalabras = this.obtenerListaDePalabras(this.categoriaSeleccionada as keyof Categorias);
+
+    this.palabraAAdivinar = this.listaDePalabras[Math.floor(Math.random() * this.listaDePalabras.length)];
+
+    this.palabraAAdivinar = this.palabraAAdivinar.toLowerCase();
+
+    this.palabraAMostrar = this.palabraAAdivinar.replace(/[a-z]/gi,'_');
+
+  }
+  obtenerListaDePalabras(categoria: keyof Categorias){
+    return this.categorias[categoria];
+  }
+
+  letraSeleccionada(letra:any){
+    console.log(this.palabraAMostrar);
+    console.log(letra, this.letrasPresionadas, this.palabraAAdivinar);
+    if(!this.letrasPresionadas.includes(letra)){
+      this.letrasPresionadas.push(letra);
+      if(!this.palabraAAdivinar.includes(letra)){
+        this.intentosRestantes--;
+      }
+      else{
+        this.actualizarPalabraMostrada();
+      }
+    }
+  }
+
+  actualizarPalabraMostrada(){
+    console.log(this.palabraAMostrar);
+
+    // this.palabraAMostrar = this.palabraAAdivinar
+    // .split('')
+    // .map(letra => this.letrasPresionadas.includes(letra) ? letra : '_')
+    // .join('');
+
+
+
+    this.palabraAMostrar = this.palabraAAdivinar.split('').map(letra =>{
+      if(letra == ' '){
+        return ' ';
+      }
+      else if( this.letrasPresionadas.includes(letra)){
+        return letra;
+      }
+      else{
+        return '_';
+      }
+    }).join('');
+
+
+    console.log(this.palabraAMostrar);
   }
 
 
--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class CategoriaService {
 
-  categoria = '';
+  categoria:any;
 
   constructor() { }
 
